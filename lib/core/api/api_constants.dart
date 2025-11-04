c8ad1e34f4c4ccabac2338af9f19c43e71ab6ab1
Magno070 <magnomicrosoftj@gmail.com>
2025-11-04 15:21:51 -0300

    api_constants changes

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String _baseUrl = 'http://localhost:3000/api';
+  static const String _baseUrl = 'http://192.168.99.74:3000/api';
   static const String _progressiveDiscounts = '$_baseUrl/progressive-discounts';
   static const String discountTables = '$_progressiveDiscounts/discounts-table';
   static const String partner = '$_progressiveDiscounts/partner';
